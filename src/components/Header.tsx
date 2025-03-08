3981bb3ea435b19d499e82fcf944f2b991ca8d82
trangman <ms.phuket@gmail.com>
2025-03-08 12:48:36 +0700

    changed logo to white

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 bg-[#1E0B4B] z-50" role="banner">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-20">
           <div className="flex items-center">
             <Link to="/" aria-label="Venture Management - Home">
               <img 
-                src="/images/Logo Silver_1.png" 
+                src="/images/Logo White_1.png" 
                 alt="Venture Management (VM) Ltd" 
-                className="h-8 sm:h-10 w-auto"
+                className="h-10 sm:h-12 md:h-14 w-auto"
               />
             </Link>
           </div>
