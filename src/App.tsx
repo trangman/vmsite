e5b9a34d5687c889d8243f9ae9e6dfbc06b70b8f
trangman <ms.phuket@gmail.com>
2025-03-08 14:30:41 +0700

    fixed a few syntax errors removed load react when not used

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ArrowRight } from 'lucide-react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
